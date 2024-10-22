db49147c0ca0aa0a24783b9a1eca7527c29f917f
Marko Golovko <marko.golovko@coraltravel.pl>
2024-10-22 11:02:10 +0200

    add hotel info to frontend

--- TravelRecommendation.Server/Hotel.cs
+++ TravelRecommendation.Server/Hotel.cs
@@ -12,12 +12,23 @@ namespace TravelRecommendation.Server
         public required string LocationId { get; set; }
         [BsonElement("name")]
         public required string Name { get; set; }
+        [BsonElement("ancestors")]
+        public required Ancestor[] Ancestors { get; set; }
         [BsonElement("brand")]
         public string? Brand { get; set; }
         [BsonElement("description")]
         public required string Description { get; set; }
         [BsonElement("Photos")]
         public List<Photos>? Photos { get; set; }
+    }
 
+    public class Ancestor
+    {
+        [BsonElement("level")]
+        public required string Level { get; set; }
+        [BsonElement("name")]
+        public required string Name { get; set; }
+        [BsonElement("location_id")]
+        public required string LocationId { get; set; }
     }
 }
--- travelrecommendation.client/src/PromptPage.tsx
+++ travelrecommendation.client/src/PromptPage.tsx
@@ -1,10 +1,30 @@
-﻿import { FormEvent, useState } from 'react';
+﻿
+import { FormEvent, useState } from 'react';
 import './App.css';
 import './PromptPage.css'
 import sendIcon from './assets/send.svg'
 
+interface Ancestor {
+    level: string;
+    name: string;
+}
+
 interface Hotel {
     name: string;
+    locationId: string;
+    ancestors: Ancestor[];
+    photos: {
+        data: {
+            album: string;
+            images: {
+                large: {
+                    height: number;
+                    width: number;
+                    url: string;
+                }
+            }
+        }[]
+    }[];
 }
 
 function PromptPage() {
@@ -19,19 +39,57 @@ function PromptPage() {
         populateHotelsData(prompt)
     }
 
+    const getHotelImage = (hotel: Hotel) => {
+        const imageObj = hotel.photos[0].data.
+            filter(image => image.album === "Hotel & Grounds")[0];
+        return <img src={imageObj.images.large.url} className="hotel-image"></img>
+    }
+
+    const getHotelLocation = (ancestors: Ancestor[]) => {
+        let country = '';
+        let region = '';
+        let city = '';
+        ancestors.forEach((ancestor: Ancestor) => {
+            switch (ancestor.level) {
+                case 'City':
+                    city = ancestor.name
+                    break;
+                case 'Municipality':
+                    city = ancestor.name
+                    break;
+                case 'Island':
+                    city = ancestor.name
+                    break;
+                case 'Region':
+                    region = ancestor.name
+                    break;
+                case 'Country':
+                    country = ancestor.name;
+                    break;
+            }
+        })
+        const ancestorsArray = [country, region, city].filter((ancestor: string) => !!ancestor)
+        return ancestorsArray.join(' / ')
+     
+    }
+
     return (
-        <div className="mt-8 flex flex-col items-center">
-            <p className="bold text-5xl">Where you want to go?</p>
-            <form className="prompt-container mt-8 w-full" onSubmit={(e) => submitPrompt(e)}>
-                <input className="w-full rounded-full p-3 pl-6 pr-12" name="prompt" />
-                <button className="flex h-8 w-8 items-center justify-center rounded-full p-0"><img src={sendIcon} className="h-6 w-6"></img></button>
+        <div className="mt-12 flex flex-col items-center">
+            <p className="bold text-8xl">Where you want to go?</p>
+            <form className="prompt-container mt-12 w-full" onSubmit={(e) => submitPrompt(e)}>
+                <input className="w-full rounded-full p-6 pl-12 pr-16" name="prompt" />
+                <button className="flex h-12 w-12 items-center justify-center rounded-full p-0"><img src={sendIcon} className="h-8 w-8"></img></button>
             </form>
             {hotels && <div>
-                <h2 className="text-3xl font-bold underline">Recommended hotels</h2>
-                <ul>
+                <h2 className="bold mt-8 text-3xl">Check this out</h2>
+                <ul className="mt-6 flex gap-10">
                     {hotels.map(hotel =>
-                        <li key={hotel.name}>
-                            {hotel.name}
+                        <li key={hotel.locationId} className="hotel-card">
+                            {getHotelImage(hotel)}
+                            <div className="p-6">
+                                <p className="text-lg">{getHotelLocation(hotel.ancestors)}</p>
+                                <p className="bold mt-3 text-4xl">{hotel.name}</p>
+                            </div>
                         </li>
                     )}
                 </ul>
