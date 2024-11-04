dc4ba0f59c3022085e87dd30d4fd9815b90f3281
Marko Golovko <marko.golovko@coraltravel.pl>
2024-11-04 09:34:56 +0100

    add FR offer rate post request

--- travelrecommendation.client/src/client/PromptPage.tsx
+++ travelrecommendation.client/src/client/PromptPage.tsx
@@ -2,22 +2,27 @@
 import { FormEvent, useState } from 'react';
 import './App.css';
 import './PromptPage.css'
-import thumb from '../assets/thumbs-up.svg';
+import HotelCard, { Hotel } from './HotelCard';
 
-interface Hotel {
-    name: string;
-    id: string;
-    location: string;
-    image: {
-        height: number;
-        width: number;
-        url: string;
-    }
-    hotelClass?: number;
-}
 
 function PromptPage() {
     const [hotels, setHotels] = useState<Hotel[]>();
+    const [prompt, setPrompt] = useState<string>('');
+
+    const populateHotelsData = async (prompt: string) => {
+        setPrompt(prompt);
+        const sendData = JSON.stringify({ 'prompt': prompt });
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: sendData,
+        };
+        const response = await fetch('hotel', requestOptions);
+        const data = await response.json();
+        setHotels(data);
+    }
 
     const submitPrompt = (e: FormEvent) => {
         e.preventDefault()
@@ -28,11 +33,17 @@ function PromptPage() {
         populateHotelsData(prompt)
     }
 
-    const star = (
-        <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" clipRule="evenodd" d="M11.0137 2.76683C11.379 1.89022 12.6208 1.89022 12.9861 2.76683L14.9102 7.38462C15.0654 7.75726 15.4295 8 15.8332 8H20.893C21.8234 8 22.2893 9.12483 21.6314 9.78268L17.5391 13.875C17.2823 14.1318 17.185 14.5076 17.2847 14.8568L18.9076 20.5369C19.1816 21.496 18.1122 22.2767 17.2822 21.7234L12.5546 18.5716C12.2187 18.3477 11.7811 18.3477 11.4452 18.5717L6.72544 21.7182C5.89284 22.2732 4.81988 21.49 5.09479 20.5279L6.71509 14.8568C6.81486 14.5076 6.71747 14.1318 6.46068 13.875L2.38859 9.8029C1.72328 9.13758 2.19448 8 3.13538 8H8.16658C8.57028 8 8.93438 7.75726 9.08965 7.38462L11.0137 2.76683Z" fill="#323232" />
-        </svg>
-    );
+    const sendRating = (rating: boolean, hotelId: string) => {
+        const sendData = JSON.stringify({ 'prompt': prompt, 'rating': rating, 'hotelId': hotelId });
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: sendData,
+        };
+        fetch('rate', requestOptions);
+    }
 
     return (
         <div className="container-size mt-12 flex flex-col items-center">
@@ -51,23 +62,8 @@ function PromptPage() {
                 <h2 className="bold mt-8 text-3xl">Check this out</h2>
                 <ul className="mt-6 flex flex-wrap justify-center gap-10">
                     {hotels.map(hotel =>
-                        <li key={hotel.id} className="hotel-card">
-                            <img src={hotel.image.url} className="hotel-image"></img>
-                            <div className="thumbs-container flex gap-3">
-                                <div className="thumb-container">
-                                    <img src={thumb} width={18} height={18} className="thumb-button"></img>
-                                </div>
-                                <div className="thumb-container">
-                                    <img src={thumb} width={18} height={18} className="thumb-button thumb-down-button"></img>
-                                </div>
-                            </div>
-                            <div className="p-6">
-                                <p className="text-lg">{hotel.location}</p>
-                                <p className="bold mt-3 text-4xl">{hotel.name}</p>
-                                <ul className="hotel-class flex gap-2">
-                                    {[...Array(hotel.hotelClass)].map((_, i) => <span key={i}>{star}</span>)}
-                                </ul>
-                            </div>
+                        <li key={hotel.id}>
+                            <HotelCard hotel={hotel} sendRating={sendRating }></HotelCard>
                         </li>
                     )}
                 </ul>
@@ -75,19 +71,7 @@ function PromptPage() {
         </div>
     );
 
-    async function populateHotelsData(prompt: string) {
-        const sendData = JSON.stringify({ 'prompt': prompt });
-        const requestOptions: RequestInit = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: sendData,
-        };
-        const response = await fetch('hotel', requestOptions);
-        const data = await response.json();
-        setHotels(data);
-    }
+
 }
 
 export default PromptPage;
