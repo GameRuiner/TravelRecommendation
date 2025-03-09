c524ae8ae2d4ee5ec3ea25505f46216bf1e1ed38
Marko Golovko <marko.golovko@coraltravel.pl>
2025-03-09 15:42:36 +0100

    add hotel_options for agent

--- travelrecommendation.client/src/client/AIWidget.tsx
+++ travelrecommendation.client/src/client/AIWidget.tsx
@@ -9,10 +9,28 @@ export default function AIWidget({agentSearch}:  {agentSearch : (prompt: string)
     ) as HTMLElement | null;
     if (widget) {
       widgetRef.current = widget;
+      const dynamicVars = {
+        hotel_options: {
+          price_level: [ '$', '$$', '$$$', '$$$$' ],
+          countries: [
+            'Spain',
+            'Greece',
+            'Egypt',
+            'Germany',
+            'Poland',
+            'United States',
+            'Japan',
+            'Turkiye',
+            'United Arab Emirates'
+          ]
+        },
+      };
+      widget.setAttribute('dynamic-variables', JSON.stringify(dynamicVars));
       widget.addEventListener('elevenlabs-convai:call', (event) => {
         const customEvent = event as CustomEvent;
         customEvent.detail.config.clientTools = {
-          createPrompt: async ({ prompt }: { prompt: string }) => {
+          createPrompt: async ({ prompt, options }: { prompt: string, options: object }) => {
+            console.log(options)
             agentSearch(prompt);
             return 'Showed hotels';
           },
